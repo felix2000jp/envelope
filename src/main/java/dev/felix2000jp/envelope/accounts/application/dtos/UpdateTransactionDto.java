f1b5055bc97cde20b8727983c0dd608c137a79df
João Félix <61743088+felix2000jp@users.noreply.github.com>
2026-03-08 18:29:03 +0000

    fix: validate decimal values (#3)
    * fix: validate decimal values
    
    * update username not found exception message

--- src/main/java/dev/felix2000jp/envelope/accounts/application/dtos/UpdateTransactionDto.java
+++ src/main/java/dev/felix2000jp/envelope/accounts/application/dtos/UpdateTransactionDto.java
@@ -1,11 +1,13 @@
 package dev.felix2000jp.envelope.accounts.application.dtos;
 
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record UpdateTransactionDto(
+        @Digits(integer = 10, fraction = 2)
         BigDecimal amount,
 
         LocalDate date,
